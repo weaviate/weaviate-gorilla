10ffbc5ed638ac3b93f170fe9c02a993f3e60ba1
Connor Shorten <connorshorten300@gmail.com>
2024-10-31 08:45:11 -0400

    Simplified query generation for collection routing

---
+++ generate_collection_routing_queries.py
@@ -0,0 +1,9 @@
+# Simplified `generate_queries`
+
+# This just generates queries that require the LLM to select one of the collections
+
+# Load Schemas JSON
+
+# For schema in schemas:
+#   For collection in schema:
+#     CreateObjects()
