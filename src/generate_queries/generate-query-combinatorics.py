a25946e1258748f2f7af5cdca2d3b4bd44ed8524
Connor Shorten <connorshorten300@gmail.com>
2024-11-08 10:34:03 -0500

    checkpoint

--- src/generate_queries/generate-query-combinatorics.py
+++ src/generate_queries/generate-query-combinatorics.py
@@ -1,4 +1,8 @@
-from api_descriptions import (
+import json
+from src.create.create import CreateObjects
+from src.lm.lm import LMService
+from src.vectorizer.vectorizer import VectorizerService
+from src.generate_queries.api_descriptions import (
     search,
     int_property_filter,
     text_property_filter,
@@ -9,14 +13,36 @@ from api_descriptions import (
     groupby
 )
 
+
 searches = [search, ""]
 filters = [int_property_filter, text_property_filter, boolean_property_filter, ""]
 aggregations = [int_property_aggregation, text_property_aggregation, boolean_property_aggregation, ""]
 groupby = [groupby, ""]
 
-# load databases
+openai_api_key = ""
 
-# for database_schema in databases
+lm_service = LMService(
+    model_provider = "openai",
+    model_name = "gpt-4o",
+    api_key = openai_api_key
+)
+
+lm_service.connection_test()
+
+vectorizer_service = VectorizerService(
+    model_provider = "openai",
+    model_name = "text-embedding-3-small",
+    api_key = openai_api_key
+)
+
+vectorizer_service.connection_test()
+
+with open("./data/simple-3-collection-schemas.json", "r") as json_file:
+    database_schemas = json.load(json_file)
+
+
+
+
 
 def CreateObjects(
         num_samples: int,
