92a4b5628e3c80511367d76e2ad14a13a23eef68
Connor Shorten <connorshorten300@gmail.com>
2024-11-08 11:26:44 -0500

    scaffold

--- src/generate_queries/generate-query-combinatorics.py
+++ src/generate_queries/generate-query-combinatorics.py
@@ -2,6 +2,7 @@ import json
 from src.create.create import CreateObjects
 from src.lm.lm import LMService
 from src.vectorizer.vectorizer import VectorizerService
+from src.models import WeaviateQuery
 from src.generate_queries.api_descriptions import (
     search,
     int_property_filter,
@@ -13,11 +14,10 @@ from src.generate_queries.api_descriptions import (
     groupby
 )
 
-
 searches = [search, ""]
 filters = [int_property_filter, text_property_filter, boolean_property_filter, ""]
 aggregations = [int_property_aggregation, text_property_aggregation, boolean_property_aggregation, ""]
-groupby = [groupby, ""]
+groupbys = [groupby, ""]
 
 openai_api_key = ""
 
@@ -37,20 +37,37 @@ vectorizer_service = VectorizerService(
 
 vectorizer_service.connection_test()
 
+# This isn't perfect, but the query operators are populated in the `references` loop internal to `CreateObjects`
+create_query_prompt = """
+Given a user's database schema write a natural language commands simulating cases where the user would want to use the provided query operators.
+
+[[ database schema ]]
+{database_schema}
+
+[[ query operators that should be used in the query ]]
+"""
+
 with open("./data/simple-3-collection-schemas.json", "r") as json_file:
     database_schemas = json.load(json_file)
 
+def format_schema(schema):
+    return json.dumps(schema, indent=2)
 
+for database_schema in database_schemas:
+    task_instructions = create_query_prompt.format(
+        database_schema=format_schema(database_schema)
+    )
+    query = CreateObjects(
+        num_samples=1,
+        task_instructions=task_instructions,
+        output_model=WeaviateQuery,
+        reference_objects=[searches, filters, aggregations, groupbys]
+        lm_service=LMService,
+        vectorizer_service=VectorizerService,
+        dedup_strategy="none"
+    )[0]
+    print(query)
 
+    # append query
 
-
-def CreateObjects(
-        num_samples: int,
-        task_instructions: str, 
-        output_model: BaseModel,
-        lm_service: LMService,
-        vectorizer_service: VectorizerService,
-        reference_objects: list[list[str]] = None,
-        dedup_strategy: str = "brute_force",
-        dedup_params: dict = None
-    ) -> list[dict]:
+# save file
