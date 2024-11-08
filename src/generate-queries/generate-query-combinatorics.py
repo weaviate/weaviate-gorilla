a677e5a340f549c74a4f79c59a24fa17651c238e
Connor Shorten <connorshorten300@gmail.com>
2024-11-08 10:28:22 -0500

    Flesh out API descriptions

--- src/generate-queries/generate-query-combinatorics.py
+++ src/generate-queries/generate-query-combinatorics.py
@@ -14,3 +14,17 @@ filters = [int_property_filter, text_property_filter, boolean_property_filter, "
 aggregations = [int_property_aggregation, text_property_aggregation, boolean_property_aggregation, ""]
 groupby = [groupby, ""]
 
+# load databases
+
+# for database_schema in databases
+
+def CreateObjects(
+        num_samples: int,
+        task_instructions: str, 
+        output_model: BaseModel,
+        lm_service: LMService,
+        vectorizer_service: VectorizerService,
+        reference_objects: list[list[str]] = None,
+        dedup_strategy: str = "brute_force",
+        dedup_params: dict = None
+    ) -> list[dict]:
