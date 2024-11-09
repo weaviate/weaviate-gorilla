9bfc79418a78e80b4e3e41c97954b30a52813e65
Connor Shorten <connorshorten300@gmail.com>
2024-11-08 21:04:00 -0500

    Move weaviate queries to data

---
+++ src/test-gorilla/test-all-Weaviate-APIs.py
@@ -0,0 +1,16 @@
+from src.models import WeaviateQuery
+from src.models import (
+    IntPropertyFilter,
+    TextPropertyFilter,
+    BooleanPropertyFilter,
+    IntAggregation,
+    TextAggregation,
+    BooleanAggregation,
+    GroupBy
+)
+from src.models import Tool, Function, Parameters, ParameterProperty
+from src.utils.weaviate_fc_utils import get_collections_info
+from src.lm.lm import LMService
+from pydantic import BaseModel
+from typing import Optional, Any
+
