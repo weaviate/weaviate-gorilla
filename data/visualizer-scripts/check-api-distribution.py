039acf91c373724d5d18dcb63bc00f78403b49bc
Connor Shorten <connorshorten300@gmail.com>
2024-11-14 11:17:22 -0500

    clean

--- data/visualizer-scripts/check-api-distribution.py
+++ data/visualizer-scripts/check-api-distribution.py
@@ -3,12 +3,12 @@ import json
 with open("../synthetic-weaviate-queries.json", "r") as f:
     data = json.load(f)
 
-print(data[0])
+print(json.dumps(data[0], indent=4))
 
 # Define the properties we want to check
 properties_to_check = [
     'search_query',
-    'integer_property_filter',
+    'integer_property_filter', 
     'text_property_filter',
     'boolean_property_filter',
     'integer_property_aggregation',
@@ -22,4 +22,17 @@ counts = {}
 for prop in properties_to_check:
     counts[prop] = sum(d[prop] is not None for d in data)
 
-print(counts)
+print(json.dumps(counts, indent=4))
+
+'''
+{
+    "search_query": 239,
+    "integer_property_filter": 334,
+    "text_property_filter": 104,
+    "boolean_property_filter": 365,
+    "integer_property_aggregation": 97,
+    "text_property_aggregation": 35,
+    "boolean_property_aggregation": 41,
+    "groupby_property": 99
+}
+'''
