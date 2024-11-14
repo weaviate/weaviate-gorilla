f984c531b090ed83aff4eeadaebf9b4ec0462fae
Connor Shorten <connorshorten300@gmail.com>
2024-11-14 13:36:05 -0500

    Haymaker

--- src/generate_queries/generate-queries-on-the-fly-models.py
+++ src/generate_queries/generate-queries-on-the-fly-models.py
@@ -1,18 +1,13 @@
-# IMPORTANT!!!
-
-# Update this script to save the source database schemas with the queries!!!
-
-# IMPORTANT!!!
-
 import json
-from src.create.create import CreateObjects
+import itertools
+from typing import Optional
+from pydantic import create_model, Field
 from src.lm.lm import LMService
 from src.vectorizer.vectorizer import VectorizerService
-from src.models import WeaviateQuery
-# save as `WeaviateQuery` models but generate with custom models created on the fly
 from src.models import (
+    WeaviateQuery,
     IntPropertyFilter,
-    TextPropertyFilter,
+    TextPropertyFilter, 
     BooleanPropertyFilter,
     IntAggregation,
     TextAggregation,
@@ -29,11 +24,6 @@ from src.generate_queries.api_descriptions import (
     boolean_property_aggregation,
     groupby
 )
-
-searches = [search_query, ""]
-filters = [int_property_filter, text_property_filter, boolean_property_filter, ""]
-aggregations = [int_property_aggregation, text_property_aggregation, boolean_property_aggregation, ""]
-groupbys = [groupby, ""]
 
 openai_api_key = ""
 
@@ -53,18 +43,18 @@ vectorizer_service = VectorizerService(
 
 vectorizer_service.connection_test()
 
-# This isn't perfect, but the query operators are populated in the `references` loop internal to `CreateObjects`
 create_query_prompt = """
-Given a user's database schema write a natural language commands simulating cases where the user would want to use the provided query operators.
+Given a user's database schema write a natural language command that requires using ALL of the following query operators to be answered correctly:
 
-Note, it is extremely important that this query is only answerable with ALL OF THE QUERY OPERATORS provided. For example, if the query is "Find me all Vegetarian dishes", you would not need a search query in addition to a Boolean `isVegetarian` filter. Please make sure these natural language queries NEED ALL OF THE REFERENCED APIS to be answered!! This is extremely important!
+{operator_description}
 
-Please note, another common mistake is to misinerpret what a `search_query` is. For example, if the query is "Show all dishes that include the word 'salad'", you can achieve this with a TEXT PROPERTY FILTER. A query requiring a `search_query` would be "find all dishes similar to arugala salad" and things like this, NOT EXACT KEYWORD MATCHES.
+The query must require ALL of these operators - do not include operators that aren't strictly necessary.
+For search queries, remember these are for semantic similarity search, not exact text matching which can be done with text property filters.
 
 [[ database schema ]]
 {database_schema}
 
-[[ query operators that should be used in the query ]]
+Please respond with a natural language query that requires all the specified operators.
 """
 
 with open("../../data/simple-3-collection-schemas.json", "r") as json_file:
@@ -73,7 +63,23 @@ with open("../../data/simple-3-collection-schemas.json", "r") as json_file:
 def format_schema(schema):
     return json.dumps(schema, indent=2)
 
-weaviate_queries = []
+# Define possible operators for each category
+search_options = [("search_query", str), None]
+filter_options = [
+    ("integer_property_filter", IntPropertyFilter),
+    ("text_property_filter", TextPropertyFilter),
+    ("boolean_property_filter", BooleanPropertyFilter),
+    None
+]
+aggregation_options = [
+    ("integer_property_aggregation", IntAggregation),
+    ("text_property_aggregation", TextAggregation),
+    ("boolean_property_aggregation", BooleanAggregation),
+    None
+]
+groupby_options = [("groupby_property", str), None]
+
+results = []
 
 import time
 start = time.time()
@@ -84,32 +90,71 @@ for idx, database_schema in enumerate(database_schemas):
     print(f"\n\033[1mRunning for {time.time() - start} seconds.\033[0m\n")
     print(f"\033[96mWriting queries for database {idx+1}:\033[0m")
     print(f"{database_schema}\n\n")
-    task_instructions = create_query_prompt.format(
-        database_schema=format_schema(database_schema)
-    )
-    queries = CreateObjects(
-        num_samples=1,
-        task_instructions=task_instructions,
-        output_model=WeaviateQuery,
-        reference_objects=[searches, filters, aggregations, groupbys],
-        lm_service=lm_service,
-        vectorizer_service=vectorizer_service,
-        dedup_strategy="none"
-    )
-    print("\033[93mQueries generated for this schema:\033[0m")
-    for query in queries:
-        # Parse the JSON string into a dict if it's a string
-        if isinstance(query, str):
-            query_dict = json.loads(query)
-        else:
-            query_dict = query
+    
+    # Generate all combinations of operators
+    for search, filter_, agg, group in itertools.product(
+        search_options, filter_options, aggregation_options, groupby_options
+    ):
+        # Skip if all are None
+        if not any([search, filter_, agg, group]):
+            continue
             
-        query_obj = WeaviateQuery(**query_dict)
-        print(f"\033[93m{query_obj}\033[0m")
-        weaviate_queries.append(query_dict)
+        # Build properties dict for dynamic model
+        properties = {
+            "corresponding_natural_language_query": (str, ...),
+            "target_collection": (str, ...)
+        }
+        
+        # Build operator description for prompt
+        operator_desc = []
+        
+        if search:
+            properties[search[0]] = (search[1], ...)
+            operator_desc.append(search_query)
+            
+        if filter_:
+            properties[filter_[0]] = (filter_[1], ...)
+            if filter_[0] == "integer_property_filter":
+                operator_desc.append(int_property_filter)
+            elif filter_[0] == "text_property_filter":
+                operator_desc.append(text_property_filter)
+            else:
+                operator_desc.append(boolean_property_filter)
+                
+        if agg:
+            properties[agg[0]] = (agg[1], ...)
+            if agg[0] == "integer_property_aggregation":
+                operator_desc.append(int_property_aggregation)
+            elif agg[0] == "text_property_aggregation":
+                operator_desc.append(text_property_aggregation)
+            else:
+                operator_desc.append(boolean_property_aggregation)
+                
+        if group:
+            properties[group[0]] = (group[1], ...)
+            operator_desc.append(groupby)
 
-    print(f"\n\033[92mCreated {len(weaviate_queries)} Gorilla queries.\033[0m\n")
+        # Create dynamic model
+        DynamicQueryModel = create_model('DynamicQueryModel', **properties)
+        
+        # Generate query using LM
+        task_instructions = create_query_prompt.format(
+            operator_description="\n".join(operator_desc),
+            database_schema=format_schema(database_schema)
+        )
+        
+        query = lm_service.generate(task_instructions, DynamicQueryModel)
+        
+        # Store result with schema
+        results.append({
+            "database_schema": database_schema,
+            "query": query.dict()
+        })
+        
+        print(f"\033[93mGenerated query: {query}\033[0m")
 
-# Save the synthetic queries to a file
-with open("synthetic-weaviate-queries.json", "w") as file:
-    json.dump(weaviate_queries, file, indent=4)
+    print(f"\n\033[92mCreated {len(results)} queries for this schema.\033[0m\n")
+
+# Save results with schemas
+with open("synthetic-weaviate-queries-with-schemas.json", "w") as file:
+    json.dump(results, file, indent=4)
