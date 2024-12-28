247acc75cba5b03171a205aec324f4636b53f83d
Connor Shorten <connorshorten300@gmail.com>
2024-12-28 16:38:54 -0500

    concept

---
+++ src/test-preference/preference-rater.py
@@ -0,0 +1,14 @@
+# Load results from serialized location (experimental-results)
+
+# Parse into:
+# Query, Claude-3.5 Sonnet Rating, GPT-4o Rating
+
+# Create some kind of Pydantic model for this?
+
+# Load LM from src.lm
+
+# Setup structured output of the preference rating for the LLM to output its preferences
+
+# Run it and log the results
+
+# Save the results
